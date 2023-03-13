cf6f5bcc3e85979e04d3117cf233f7d00b86443b
Alejandro Aguado <55259054+Iber1to@users.noreply.github.com>
2023-03-13 12:35:04 +0100

    Initial Upload

---
+++ Active Directory/Export-GPOtoXML.ps1
@@ -0,0 +1,11 @@
+$ou = "OU=Workstations,DC=contoso,DC=Intcontoso,DC=LOCAL"
+
+$lisnkedGPOs = Get-ADOrganizationalUnit -LDAPFilter '(name=*)' -SearchBase $ou -SearchScope Subtree | Select-object -ExpandProperty LinkedGroupPolicyObjects
+
+$LinkedGPOGUIDs = $lisnkedGPOs | ForEach-object{$_.Substring(4,36)}
+
+$listGPOs = $LinkedGPOGUIDs | ForEach-object {Get-GPO -Guid $_ | Select-object DisplayName}
+
+$listGposUnique =$listGPOs | Select-Object -Property DisplayName -Unique
+
+$listGposUnique.ForEach({Get-GPOReport -Name $_.DisplayName -ReportType Xml -Path "C:\Temp\GpoWorkstation\$($_.DisplayName).xml"})

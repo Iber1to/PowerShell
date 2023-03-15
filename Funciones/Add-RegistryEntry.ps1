5a2ff8322db6b04cf8db83bf9a5e19d5a6e31ec3
Alejandro Aguado <55259054+Iber1to@users.noreply.github.com>
2023-03-15 13:24:59 +0100

    Update version and comments

--- Funciones/Add-RegistryEntry.ps1
+++ Funciones/Add-RegistryEntry.ps1
@@ -15,17 +15,22 @@
     The value of the registry entry.
 
 .PARAMETER ValueType
-    The type of the registry entry. Valid values are "String", "Binary", "DWord", "MultiString", "ExpandString", "QWord", and "unknown".
+    The type of the registry entry. Valid values are "String" (REG_SZ), "Binary" (REG_BINARY), "DWord" (REG_DWORD), "MultiString" (REG_MULTI_SZ), "ExpandString" (REG_EXPAND_SZ), "QWord" (REG_QWORd), and "unknown" (REG_SZ).
 
 .EXAMPLE
     Add-RegistryEntry -Path "HKCU:\Software\MyApp" -RegistryName "MyEntry" -Value "MyValue" -ValueType "String"
     This command adds a new registry entry named "MyEntry" with the value "MyValue" to the path "HKCU:\Software\MyApp".
 
+.VERSION
+    1.0.1 15-03-2023
+    - Modify the function to return a message if the registry entry already exists.
+    - Modify comments "ValueType" to show the name values of the registry entry types as they appear in the registry editor.    
+
 .NOTES
     Autor: Alejandro Aguado García
     Fecha de creación: 01-03-2023   
     Última modificación: 01-03-2023
-    Versión:  1.0.0
+    Versión:  1.0.1
     Linkedin: https://www.linkedin.com/in/alejandro-aguado-08882a31/
     Github:   https://github.com/Iber1to
     Twitter:  @Alejand94399487
@@ -47,14 +52,17 @@ function Add-RegistryEntry {
         try{
             New-Item -Path $pathFull -Force | Out-Null
         } catch {
-            return "Unable to create registry key $pathFull"
+            return Write-Output "Unable to create registry key $pathFull"
         }
     }
     if (-not (Get-ItemProperty -Path $pathFull -Name $RegistryName -ErrorAction SilentlyContinue)) {
         try{
             New-ItemProperty -Path $pathFull -Name $RegistryName -Value $Value -PropertyType $ValueType -Force | Out-Null
         } catch {
-            return "Unable to create registry entry $RegistryName"
+            return Write-Output "Unable to create registry entry $RegistryName"
         }
+    }
+    else {
+        return Write-Output "Registry entry $RegistryName already exists"
     }
 }
--- Funciones/Test-Registry.ps1
+++ Funciones/Test-Registry.ps1
@@ -1,14 +1,47 @@
+<#
+.SYNOPSIS
+    Verifies the existence of a registry key or value on the system.
+
+.DESCRIPTION
+    The Test-Registry function checks the existence of a registry key or value on the system, using the registry hive, the key path, and optionally, a specific value.
+
+.PARAMETER hive
+    The registry hive location. Accepts one of the following values: "HKLM:\", "HKCU:\", "HKCR:\", "HKU:\" or "HKCC:\".
+
+.PARAMETER path
+    The registry key path you want to verify.
+
+.PARAMETER RegistryName
+    Optional. The name of the registry value you want to verify within the specified key. If not provided, the function will only check the existence of the registry key.
+
+.EXAMPLE
+    Test-Registry -hive "HKLM:\" -path "Software\MyCompany\MyApp" -RegistryName "Version"
+    This example verifies the existence of the "Version" value in the registry key "HKLM:\Software\MyCompany\MyApp". It returns $true if the value exists, and $false otherwise.
+
+    Test-Registry -hive HKCU:\ -Path "Software\Policies\Microsoft\Office\16.0\Outlook\Options\Mail"
+    This example verifies the existence of the registry key "HKCU:\Software\Policies\Microsoft\Office\16.0\Outlook\Options\Mail". It returns $true if the key exists, and $false otherwise.
+
+.NOTES
+    Author: Alejandro Aguado García
+    Creation date: 15-03-2023
+    Last modification: 15-03-2023
+    Version:  1.0.0
+    LinkedIn: https://www.linkedin.com/in/alejandro-aguado-08882a31/
+    GitHub:   https://github.com/Iber1to
+    Twitter:  @Alejand94399487
+#>
+
 function Test-Registry {
     param (
         [ValidateSet("HKLM:\", "HKCU:\", "HKCR:\", "HKU:\", "HKCC:\")]
         [string]$hive,
         [string]$path,
-        [string]$value
+        [string]$RegistryName
     )
     $pathFull = $hive + $path
     if($value){
         try{
-            $result = get-itemproperty -path $pathFull -name $value -ea 0
+            $result = get-itemproperty -path $pathFull -name $RegistryName -ea 0
             if($result){
                 return $true
             }

c0380929ce982956b87f4bdff712dc8554125262
Iber0 <alejandro.aguado@gmail.com>
2023-06-23 12:31:46 +0200

    New

---
+++ Azure Active Directory/get-AzureDeviceIDtoBulkImport.ps1
@@ -0,0 +1,31 @@
+#Parametros
+#Este fichero deber ser el export devices de Azure y tiene que contener las columnas "displayName" y "objectId"
+$ficheroAzureCsv = "C:\Users\aleja\Downloads\exportDevice_2023-6-23.csv"
+#El fichero debe contener una columna "displayName" con el nombre del dispositivo
+$ficheroDevicesMatch = "C:\Users\aleja\Downloads\test1.csv"
+#Fichero de salida para el csv
+$ficheroSalida = "C:\temp\devicesupgrade14.csv"
+#Strings para el export-csv
+$string1 = "version:v1.0"
+$string2 = "Member object ID or user principal name [memberObjectIdOrUpn] Required"
+
+#Inicializo Arrays
+$azureObjects = [System.Collections.ArrayList]::new()
+$matchObjets = [System.Collections.ArrayList]::new()
+$finalObjetcs = [System.Collections.ArrayList]::new()
+
+#Cargo archivos con datos
+Import-csv $ficheroAzureCsv | ForEach-Object {$azureObjects.add($_)} |Out-Null
+Import-csv $ficheroDevicesMatch | ForEach-Object {$matchObjets.add($_)} |Out-Null
+
+#Cargo strings al Output
+$finalObjetcs.Add($string1)
+$finalObjetcs.Add($string2)
+
+foreach($item in $matchObjets){
+    $Matchuser = $azureObjects.where({$_.displayName -eq $item.displayName})
+    if($Matchuser){$finalObjetcs.Add($Matchuser.objectId)
+    }
+}
+
+$finalObjetcs | Out-File $ficheroSalida -Force

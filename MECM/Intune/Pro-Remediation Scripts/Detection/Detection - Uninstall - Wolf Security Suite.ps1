0af399da64596e173851e081cbabdd5c907cb6c7
Iber0 <alejandro.aguado@gmail.com>
2024-03-19 16:21:58 +0100

    Modify logging

--- MECM/Intune/Pro-Remediation Scripts/Detection/Detection - Uninstall - Wolf Security Suite.ps1
+++ MECM/Intune/Pro-Remediation Scripts/Detection/Detection - Uninstall - Wolf Security Suite.ps1
@@ -1,26 +1,83 @@
-#region "Transcript parameters"
-$logpath  = 'C:\Windows\Temp'
+#region log
+function Write-CMTracelog {
+    [CmdletBinding()]
+    Param(
+
+          [Parameter(Mandatory=$true)]
+          [String]$Message,
+            
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [String]$Path,
+                  
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [String]$Component,
+
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [ValidateSet("Information", "Warning", "Error")]
+          [String]$Type = 'Information'
+    )
+
+    if(!$Path){
+        $Path= $PathCMTracelog
+        }
+    if(!$Component){
+        $Component= $ComponentSource
+        }
+        
+    switch ($Type) {
+        "Info" { [int]$Type = 1 }
+        "Warning" { [int]$Type = 2 }
+        "Error" { [int]$Type = 3 }
+    }
+
+    # Create a CMTrace formatted entry
+    $Content = "<![LOG[$Message]LOG]!>" +`
+        "<time=`"$(Get-Date -Format "HH:mm:ss.ffffff")`" " +`
+        "date=`"$(Get-Date -Format "M-d-yyyy")`" " +`
+        "component=`"$Component`" " +`
+        "context=`"$([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)`" " +`
+        "type=`"$Type`" " +`
+        "thread=`"$([Threading.Thread]::CurrentThread.ManagedThreadId)`" " +`
+        "file=`"`">"
+
+    # Add the line to the log file   
+    Add-Content -Path $Path -Value $Content
+}
+#region "log parameters"
+$logpath  = 'C:\Windows\Logs\Uninstall\'
 $username   = $env:USERNAME
 $hostname   = hostname
-$version    = $PSVersionTable.PSVersion.ToString()
 $datetime   = Get-Date -f 'yyyyMMddHHmmss'
-$scriptname = $MyInvocation.MyCommand.Name.Replace('.ps1', '') # Get file name without extension .ps1
-$filename   = "Transcript-${scriptname}-${username}-${hostname}-${version}-${datetime}.txt"
-$Transcript = Join-Path -Path $logpath -ChildPath $filename
-#endregion "Transcript parameters"
+$scriptname = "Invoke-CustomInventory"
+$filename   = "${scriptname}-${username}-${hostname}-${datetime}.log"
+$logfilename = Join-Path -Path $logpath -ChildPath $filename
+$PathCMTracelog = $logfilename
+$ComponentSource = $MyInvocation.MyCommand.Name
+#endregion "log parameters"
+# Test logpath
+if(-not (Test-Path $logpath)){
+	New-Item -ItemType Directory -Path $logpath -Force
+}
+#endregion log
 
 #region "Procces"
-Start-Transcript -Path $Transcript
+Write-CMTracelog "Start execution: ${scriptname}"
+Write-CMTracelog "Searching for Apps"
 $App001 = Get-Package | Where-Object {$_.Name -eq 'HP Wolf Security'}
 $App002 = Get-Package | Where-Object {$_.Name -eq "HP Wolf Security - Console"}
 $App003 = Get-Package | Where-Object {$_.Name -eq "HP Security Update Service"}
 if ($App001 -or $App002 -or $App003) {
-    Stop-Transcript
     Write-Host "App detected"
+    Write-CMTracelog "App detected"
+    Write-CMTracelog "End execution: ${scriptname}"
     exit 1
 }else{
-    Stop-Transcript
+    Write-CMTracelog "Not App detected"
     Write-Host "Not App detected"
+    Write-CMTracelog "End execution: ${scriptname}"
     exit 0
 }
 #endregion "Procces"
--- MECM/Intune/Pro-Remediation Scripts/Remediation/Remediation - Uninstall - Wolf Security Suite.ps1
+++ MECM/Intune/Pro-Remediation Scripts/Remediation/Remediation - Uninstall - Wolf Security Suite.ps1
@@ -1,31 +1,85 @@
-#region "Transcript parameters"
-$logpath  = 'C:\Windows\Temp'
+#region log
+function Write-CMTracelog {
+    [CmdletBinding()]
+    Param(
+
+          [Parameter(Mandatory=$true)]
+          [String]$Message,
+            
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [String]$Path,
+                  
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [String]$Component,
+
+          [Parameter()]
+          [ValidateNotNullOrEmpty()]
+          [ValidateSet("Information", "Warning", "Error")]
+          [String]$Type = 'Information'
+    )
+
+    if(!$Path){
+        $Path= $PathCMTracelog
+        }
+    if(!$Component){
+        $Component= $ComponentSource
+        }
+        
+    switch ($Type) {
+        "Info" { [int]$Type = 1 }
+        "Warning" { [int]$Type = 2 }
+        "Error" { [int]$Type = 3 }
+    }
+
+    # Create a CMTrace formatted entry
+    $Content = "<![LOG[$Message]LOG]!>" +`
+        "<time=`"$(Get-Date -Format "HH:mm:ss.ffffff")`" " +`
+        "date=`"$(Get-Date -Format "M-d-yyyy")`" " +`
+        "component=`"$Component`" " +`
+        "context=`"$([System.Security.Principal.WindowsIdentity]::GetCurrent().Name)`" " +`
+        "type=`"$Type`" " +`
+        "thread=`"$([Threading.Thread]::CurrentThread.ManagedThreadId)`" " +`
+        "file=`"`">"
+
+    # Add the line to the log file   
+    Add-Content -Path $Path -Value $Content
+}
+#region "log parameters"
+$logpath  = 'C:\Windows\Logs\Uninstall\'
 $username   = $env:USERNAME
 $hostname   = hostname
-$version    = $PSVersionTable.PSVersion.ToString()
 $datetime   = Get-Date -f 'yyyyMMddHHmmss'
-$scriptname = $MyInvocation.MyCommand.Name.Replace('.ps1', '') # Get file name without extension .ps1
-$filename   = "Transcript-${scriptname}-${username}-${hostname}-${version}-${datetime}.txt"
-$Transcript = Join-Path -Path $logpath -ChildPath $filename
-#endregion "Transcript parameters"
+$scriptname = "Invoke-CustomInventory"
+$filename   = "${scriptname}-${username}-${hostname}-${datetime}.log"
+$logfilename = Join-Path -Path $logpath -ChildPath $filename
+$PathCMTracelog = $logfilename
+$ComponentSource = $MyInvocation.MyCommand.Name
+#endregion "log parameters"
+# Test logpath
+if(-not (Test-Path $logpath)){
+	New-Item -ItemType Directory -Path $logpath -Force
+}
+#endregion log
 
 #region "Procces script"
-Start-Transcript -Path $Transcript
+Write-CMTracelog "Start execution: ${scriptname}" 
  
-try{Start-Process -filepath "wmic" -argumentlist 'product where name="HP Wolf Security" call uninstall' -wait -WindowStyle Hidden}
-catch{ 
-    Write-Host "$($_.Exception.Message)"
-    Stop-Transcript
-    }
-try{Start-Process -filepath "wmic" -argumentlist 'product where name="HP Wolf Security - Console" call uninstall' -Wait -WindowStyle Hidden}
-catch{ 
-    Write-Host "$($_.Exception.Message)"
-    Stop-Transcript
-    }
-try{Start-Process -filepath "wmic" -argumentlist 'product where name="HP Security Update Service" call uninstall' -Wait -WindowStyle Hidden}
-catch{ 
-    Write-Host "$($_.Exception.Message)"
-    Stop-Transcript
-    }
-Stop-Transcript
+try{
+    Write-CMTracelog "Uninstalling HP Wolf Security"
+    Start-Process -filepath "wmic" -argumentlist 'product where name="HP Wolf Security" call uninstall' -wait -WindowStyle Hidden
+    Write-CMTracelog "Uninstalling HP Wolf Security Completed" -Type Warning
+    }catch{Write-CMTracelog "$($_.Exception.Message)" -Component "WMIC" -Type Error}
+try{
+    Write-CMTracelog "Uninstalling HP Wolf Security - Console" 
+    Start-Process -filepath "wmic" -argumentlist 'product where name="HP Wolf Security - Console" call uninstall' -Wait -WindowStyle Hidden
+    Write-CMTracelog "Uninstalling HP Wolf Security - Console Completed" -Type Warning
+    }catch{Write-CMTracelog "$($_.Exception.Message)" -Component "WMIC" -Type Error}
+try{
+    Write-CMTracelog "Uninstalling HP Security Update Service"
+    Start-Process -filepath "wmic" -argumentlist 'product where name="HP Security Update Service" call uninstall' -Wait -WindowStyle Hidden
+    Write-CMTracelog "Uninstalling HP Security Update Service Completed" -Type Warning
+    }catch{ Write-CMTracelog "$($_.Exception.Message)" -Component "WMIC" -Type Error}
+
 #endregion "Procces"
